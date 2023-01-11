d40fdcee595afe489c30d874d8ea6fd8c5175466
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2023-01-10 16:21:13 -0800

    route for submissions

--- routes.py
+++ routes.py
@@ -1,8 +1,43 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, request, jsonify, make_response
+from dotenv import dotenv_values
+import psycopg2
 
 api = Blueprint('api', __name__)
 
+def get_env(var):
+  return dotenv_values(".env")[var]
+
+def get_db_connection():
+  conn = psycopg2.connect(
+          host='localhost',
+          database=get_env("DB"),
+          user=get_env("DB_USERNAME"),
+          password=get_env("DB_PASSWORD"))
+  return conn
+
+
 @api.route('/add', methods=['POST'])
 def add_artist():
-  print(request.json)
-  return request.json
+  conn = get_db_connection()
+  cur = conn.cursor()
+
+  genre = request.json['genre']
+  name = request.json['artist']
+  url = request.json['url']
+
+  cur.execute('INSERT INTO public.artists (genre_id, name, spotify_url)'
+           'VALUES (%s, %s, %s) RETURNING id', (genre, name, url))
+
+  conn.commit()
+
+  artists = cur.fetchall()
+
+  if artists is None:
+    return jsonify({"status": "500"})
+
+  cur.close()
+  conn.close()
+
+  response = make_response(jsonify({ "status": 200, "data": name }))
+
+  return response
