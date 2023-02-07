d38e6c647e3414f1918addfad453e756c005ceba
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2023-02-07 12:52:22 -0800

    updating db queries

--- routes.py
+++ routes.py
@@ -15,18 +15,28 @@ def get_db_connection():
           password=get_env("DB_PASSWORD"))
   return conn
 
+@api.route('/test', methods=['POST'])
+def test_request():
+  print('hello world')
+  response = make_response(jsonify({ "status": 200, "data": "hi" }))
+  return response
+
 
 @api.route('/add', methods=['POST'])
 def add_artist():
   conn = get_db_connection()
   cur = conn.cursor()
 
-  genre = request.json['genre']
   name = request.json['artist']
   url = request.json['url']
+  uri = request.json['uri']
+  id = request.json['id']
+  match_1 = request.json['match1']
+  match_2 = request.json['match2']
+  match_3 = request.json['match3']
 
-  cur.execute('INSERT INTO public.artists (genre_id, name, spotify_url)'
-           'VALUES (%s, %s, %s) RETURNING id', (genre, name, url))
+  cur.execute('INSERT INTO public.artist_matches (spotify_id, name, spotify_url, spotify_uri, match_1, match_2, match_3)'
+           'VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id', (id, name, url, uri, match_1, match_2, match_3))
 
   conn.commit()
 
@@ -39,5 +49,29 @@ def add_artist():
   conn.close()
 
   response = make_response(jsonify({ "status": 200, "data": name }))
+
+  return response
+
+@api.route('/add_email', methods=['POST'])
+def add_email():
+  conn = get_db_connection()
+  cur = conn.cursor()
+
+  email = request.json['email']
+
+  cur.execute('INSERT INTO public.emails (email)'
+              'VALUES (%s) RETURNING email', (email,))
+
+  conn.commit()
+
+  email_added = cur.fetchall()
+
+  if email_added is None:
+    return jsonify({"status": "500"})
+
+  cur.close()
+  conn.close()
+
+  response = make_response(jsonify({ "status": 200, "data": email }))
 
   return response
